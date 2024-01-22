2840d5718769670466286cc874be0aa6a7eb05c7
Muhammad Ikhwan <mikhwan1206@gmail.com>
2024-01-22 18:09:43 +0700

    update file user dan kategori

--- resources/views/Kategoribuku/edit.blade.php
+++ resources/views/Kategoribuku/edit.blade.php
@@ -1,11 +1,38 @@
-<form action="{{ route('kategoribuku.update', $dataeditkategori->KategoriID)}}" method="POST">
-    {{ csrf_field() }}
-    <input type="hidden" name="_method" value="PUT" />
-    <div class="form-group">
-        <label for="NamaKategori" class="form-label">Nama Kategori</label>
-        <input type="text" class="form-control" placeholder="Nama Kategori" name="NamaKategori" value="{{$dataeditkategori->NamaKategori}}">
+@extends('layouts.main')
+@section('content')
+<div class="row">
+    <div class="col-xxl">
+        <div class="card mb-4">
+            <div class="card-header d-flex align-items-center justify-content-between">
+                <h5 class="mb-0">Ubah Kategori Buku</h5>
+                <small class="text-muted float-end"></small>
+            </div>
+            <div class="card-body">
+                <form action="{{ route('kategoribuku.update', $dataeditkategori->KategoriID)}}" method="POST">
+                    {{ csrf_field() }}
+                    <input type="hidden" name="_method" value="PUT" />
+                    <div class="row mb-3">
+                        <label for="NamaKategori" class="col-sm-2 col-form-label">Nama Kategori</label>
+                        <div class="col-sm-10">
+                            <input type="text" class="form-control" placeholder="Nama Kategori" name="NamaKategori"
+                                value="{{$dataeditkategori->NamaKategori}}">
+                            @error('NamaKategori')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                            @enderror
+                        </div>
+                    </div>
+                    <div class="row justify-content-end">
+                        <div class="col-sm-10">
+                            <button type="submit" class="btn btn-primary">Ubah</button>
+                            <a href="/kategoribuku" class="btn btn-dark">Kembali</a>
+                        </div>
+                    
+                    </div>
+                </form>
+            </div>
+        </div>
     </div>
-    <div class="col-12">
-        <button type="submit" class="btn btn-primary">Ubah</button>
-    </div>
-</form>
+</div>
+@endsection
--- resources/views/User/create.blade.php
+++ resources/views/User/create.blade.php
@@ -1,74 +1,105 @@
-<form method="POST" action="{{ route('user.store') }}">
-    {{ csrf_field() }}
-    <div class="form-group">
-        <label for="Username" class="form-label">{{ __('Username') }}</label>
-        <input id="Username" type="text" class="form-control @error('Username') is-invalid @enderror" name="Username"
-            value="{{ old('Username') }}" required autocomplete="Username" autofocus>
+@extends('layouts.main')
+@section('content')
+<div class="row">
+    <div class="col-xxl">
+        <div class="card mb-4">
+            <div class="card-header d-flex align-items-center justify-content-between">
+                <h5 class="mb-0">Tambah User</h5>
+                <small class="text-muted float-end"></small>
+            </div>
+            <div class="card-body">
+                <form method="POST" action="{{ route('user.store') }}">
+                    {{ csrf_field() }}
+                    <div class="row mb-3">
+                        <label for="Username" class="col-sm-2 col-form-label">{{ __('Username') }}</label>
 
-        @error('Username')
-        <span class="invalid-feedback" role="alert">
-            <strong>{{ $message }}</strong>
-        </span>
-        @enderror
-    </div>
+                        <div class="col-sm-10">
+                            <input id="Username" type="text"
+                                class="form-control @error('Username') is-invalid @enderror" name="Username"
+                                value="{{ old('Username') }}" required autocomplete="Username" autofocus>
 
-    <div class="form-group">
-        <label for="Namalengkap" class="form-label">{{ __('Nama Lengkap') }}</label>
-        <input id="Namalengkap" type="text" class="form-control @error('Namalengkap') is-invalid @enderror"
-            name="Namalengkap" value="{{ old('Namalengkap') }}" required autocomplete="Namalengkap" autofocus>
+                            @error('Username')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
 
-        @error('Namalengkap')
-        <span class="invalid-feedback" role="alert">
-            <strong>{{ $message }}</strong>
-        </span>
-        @enderror
+                            @enderror
+                        </div>
+                    </div>
 
-    </div>
+                    <div class="row mb-3">
+                        <label for="Namalengkap" class="col-sm-2 col-form-label">{{ __('Nama Lengkap') }}</label>
 
-    <div class="form-group">
-        <label for="Email" class="form-label">{{ __('E-Mail') }}</label>
-        <input id="Email" type="email" class="form-control @error('Email') is-invalid @enderror" name="Email"
-            value="{{ old('Email') }}" required autocomplete="Email">
+                        <div class="col-sm-10">
+                            <input id="Namalengkap" type="text"
+                                class="form-control @error('Namalengkap') is-invalid @enderror" name="Namalengkap"
+                                value="{{ old('Namalengkap') }}" required autocomplete="Namalengkap" autofocus>
 
-        @error('Email')
-        <span class="invalid-feedback" role="alert">
-            <strong>{{ $message }}</strong>
-        </span>
-        @enderror
+                            @error('Namalengkap')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                            @enderror
+                        </div>
 
-    </div>
+                    </div>
 
-    <div class="form-group">
-        <label for="Password" class="form-label">{{ __('Password') }}</label>
-        <input id="Password" type="password" class="form-control @error('Password') is-invalid @enderror"
-            name="Password" required autocomplete="new-password">
+                    <div class="row mb-3">
+                        <label for="Email" class="col-sm-2 col-form-label">{{ __('E-Mail') }}</label>
+                        <div class="col-sm-10">
+                            <input id="Email" type="email" class="form-control @error('Email') is-invalid @enderror"
+                                name="Email" value="{{ old('Email') }}" required autocomplete="Email">
 
-        @error('Password')
-        <span class="invalid-feedback" role="alert">
-            <strong>{{ $message }}</strong>
-        </span>
-        @enderror
+                            @error('Email')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                            @enderror
+                        </div>
 
-    </div>
+                    </div>
 
-    <div class="form-group">
-        <label for="Alamat" class="form-label">{{ __('Alamat') }}</label>
-        <textarea id="Alamat" type="password" class="form-control @error('Alamat') is-invalid @enderror" name="Alamat"
-            required ></textarea>
-    
-        @error('Password')
-        <span class="invalid-feedback" role="alert">
-            <strong>{{ $message }}</strong>
-        </span>
-        @enderror
-    
-    </div>
+                    <div class="row mb-3">
+                        <label for="Password" class="col-sm-2 col-form-label">{{ __('Password') }}</label>
+                        <div class="col-sm-10">
+                            <input id="Password" type="password"
+                                class="form-control @error('Password') is-invalid @enderror" name="Password" required
+                                autocomplete="new-password">
 
-    <div class="form-group mb-0">
-        <div class="col-md-6 offset-md-4">
-            <button type="submit" class="btn btn-primary">
-                {{ __('Simpan') }}
-            </button>
+                            @error('Password')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                            @enderror
+                        </div>
+                    </div>
+
+                    <div class="row mb-3">
+                        <label for="Alamat" class="col-sm-2 col-form-label">{{ __('Alamat') }}</label>
+                        <div class="col-sm-10">
+                            <textarea id="Alamat" type="password"
+                                class="form-control @error('Alamat') is-invalid @enderror" name="Alamat"
+                                required>{{ old('Alamat') }}</textarea>
+
+                            @error('Password')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                            @enderror
+                        </div>
+                    </div>
+
+                    <div class="row justify-content-end">
+                        <div class="col-sm-10">
+                            <button type="submit" class="btn btn-primary">Simpan</button>
+                            <a href="/user" class="btn btn-dark">Kembali</a>
+                        </div>
+
+                    </div>
+
+                </form>
+            </div>
         </div>
     </div>
-</form>
+</div>
+@endsection
