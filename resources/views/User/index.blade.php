ff7df9320db4a3b879630d61bf3ec8e6aa8fee29
Muhammad Ikhwan <mikhwan1206@gmail.com>
2024-01-22 16:32:56 +0700

    update index user

--- resources/views/User/index.blade.php
+++ resources/views/User/index.blade.php
@@ -1,38 +1,51 @@
 @extends('layouts.main')
 @section('content')
-<div class="col-12">
-    <a href="{{ route('user.create')}}" class="btn btn-primary">Tambah Data User</a>
+<div class="card">
+    <div class="card-header d-flex justify-content-between align-items-center">
+        <h5 class="mb-0">Daftar Data User</h5>
+        <div class="d-flex ">
+            <a href="{{ route('user.create')}}" class="btn btn-dark btn-sm me-2">Tambah Data User</a>
+        </div>
+    </div>
+    @if(session('success'))
+    <div class="alert alert-success alert-dismissible" role="alert" id="notification">
+        {{ session('success') }}
+        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+    </div>
+    @endif
+    <div class="table-responsive text-nowrap">
+        <table class="table">
+            <thead>
+                <tr>
+                    <th>#</th>
+                    <th>Username</th>
+                    <th>Nama Lengkap</th>
+                    <th>Email</th>
+                    <th>Alamat</th>
+                    <th>Role</th>
+                    <th>Aksi</th>
+                </tr>
+            </thead>
+            <tbody>
+                @forelse ( $dataUser as $user)
+                <tr>
+                    <td>{{ $loop->iteration }}</td>
+                    <td>{{ $user->Username}}</td>
+                    <td>{{ $user->Namalengkap }}</td>
+                    <td>{{ $user->Email}}</td>
+                    <td>{{ $user->Alamat }}</td>
+                    <td>{{ $user->Role}}</td>
+                    <td>
+                        <a href="{{route('user.edit',$user->UserID)}}" class="btn btn-primary">Ubah</a>
+                    </td>
+                </tr>
+                @empty
+                <tr>
+                    <td colspan="8">Data User Kosong.</td>
+                </tr>
+                @endforelse
+            </tbody>
+        </table>
+    </div>
 </div>
-<table class="table">
-    <thead>
-        <tr>
-            <th>#</th>
-            <th>Username</th>
-            <th>Nama Lengkap</th>
-            <th>Email</th>
-            <th>Alamat</th>
-            <th>Role</th>
-            <th>Aksi</th>
-        </tr>
-    </thead>
-    <tbody>
-        @forelse ( $dataUser as $user)
-        <tr>
-            <td>{{ $loop->iteration }}</td>
-            <td>{{ $user->Username}}</td>
-            <td>{{ $user->Namalengkap }}</td>
-            <td>{{ $user->Email}}</td>
-            <td>{{ $user->Alamat }}</td>
-            <td>{{ $user->Role}}</td>
-            <td>
-                <a href="{{route('user.edit',$user->UserID)}}" class="btn btn-primary">Ubah</a>
-            </td>
-        </tr>
-        @empty
-        <tr>
-            <td colspan="8">Data User Kosong.</td>
-        </tr>
-        @endforelse
-    </tbody>
-</table>
 @endsection
